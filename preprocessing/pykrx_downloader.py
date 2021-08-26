b539e3216bed0e989e7f6d3f7b5613561dbd08e4
Undefined-K <undefined.kim@gmail.com>
2021-08-26 16:41:53 +0900

    pykrx에서 시장 종목 다운로드 하는 코드 추가, 종목 리스트 엑셀 업로드 (1초 수신 제한으로 다운로드 시간이 꽤 걸려서 미리 받아 놓음)

--- preprocessing/pykrx_downloader.py
+++ preprocessing/pykrx_downloader.py
@@ -1,2 +1,23 @@
 from pykrx import stock
+import pandas as pd
+import time
+import os
+
+width = 320
+pd.set_option('display.width', width)
+
+if not os.path.exists('./data/market_stock_list.csv'):
+    print("stock list downloading..")
+    stock_list_df = pd.DataFrame(columns=['시장', '티커', '종목이름'])
+    stock_market = ['KOSPI', 'KOSDAQ', 'KONEX']
+
+    for stock_market_element in stock_market:
+        for ticker in stock.get_market_ticker_list(market=stock_market_element):
+            종목이름 = stock.get_market_ticker_name(ticker)
+            stock_list_df = stock_list_df.append(pd.Series([stock_market_element, ticker, 종목이름], index=stock_list_df.columns),
+                                             ignore_index=True)
+            print(stock_market_element, ticker, 종목이름)
+            time.sleep(1)
+
+    stock_list_df.to_csv('./data/market_stock_list.csv')
 
